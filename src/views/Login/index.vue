2ee348e57ab018b4e1a17aaed8a4abf9829ef7b1
leshihu <leshihu@outlook.com>
2020-01-22 18:11:06 +0800

    构建项目、配置全局样式、路由、elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login">
+        这是登录页面
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+.login{
+    height: 100vh;
+    background-color:#344a5f;
+}
+</style>
